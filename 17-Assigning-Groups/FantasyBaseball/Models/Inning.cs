61b6ba5ecca4e384620f275556aaef74d48c4c95
Amanda Neumann <amandaneumann@me.com>
2014-07-01 10:12:45 +1000

    Lesson 17: Assigning Groups

---
+++ 17-Assigning-Groups/FantasyBaseball/Models/Inning.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FantasyBaseball.Models
+{
+    public enum HalfInning {
+        Top,
+        Bottom
+    }
+
+    public class Inning {
+        private int _inning;
+        private HalfInning _halfInning;
+
+        public Inning( ) {
+            _halfInning = HalfInning.Top;
+            _inning = 1;
+        }
+
+        public HalfInning HalfInning {
+            get { return _halfInning; }
+        }
+
+        public void Advance( ) {
+            if(_halfInning == HalfInning.Top)
+                _halfInning = HalfInning.Bottom;
+            else {
+                _halfInning = HalfInning.Top;
+                _inning++;
+            }
+        }
+
+        public override string ToString( ) {
+            var inningString = string.Empty;
+            var lastInningDigit = _inning%10;
+
+            switch ( lastInningDigit ) {
+                case 1:
+                    if ( _inning == 11 )
+                        inningString = _inning + "th";
+                    else
+                        inningString = _inning + "st";
+                    break;
+                case 2:
+                    inningString = _inning + "nd";
+                    break;
+                case 3:
+                    inningString = _inning + "rd";
+                    break;
+                default:
+                    inningString = _inning + "th";
+                    break;
+            }
+
+            return string.Format( "{0} of the {1}", _halfInning.ToString( ), inningString );
+        }
+    }
+}
