0b50850e49e640ac6bee321d785ca74e43aec34a
Amanda Neumann <amandaneumann@me.com>
2014-07-01 10:08:40 +1000

    Lesson 12: Building our models

---
+++ 12-Building-Our-Models/FantasyBaseball/Models/IdentityModels.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using FantasyBaseball.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace FantasyBaseball.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public virtual ICollection<FavoriteTeam> FavoriteTeams { get; set; } 
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<FavoriteTeam> FavoriteTeams { get; set; } 
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+    }
+}
